765835d5b2cb4e8b304eceb16bd9a4bc11771781
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-01-27 11:16:50 +0530

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
