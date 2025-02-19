a8c0e2e7107cd17d481b7ae042847a67d7779dcd
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-02-19 14:33:27 +0530

    mongo

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/mongoDB.js';
+import connectDB from './config/mongodb.js';
 import connectCloudinary from './config/cloudinary.js';
 import adminRouter from './routes/adminRoute.js';
 import doctorRouter from './routes/doctorRoute.js';
