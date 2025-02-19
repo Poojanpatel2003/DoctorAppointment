80884e60bcda0840244b67eedc4ed46ecd236037
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-02-19 23:14:34 +0530

    hashrouter

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,49 +1,51 @@
-import { lazy, Suspense, useContext } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/login";
 import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import 'react-toastify/dist/ReactToastify.css';
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
-
-const Dashboard = lazy(() => import("./pages/Admin/Dashboard"));
-const AllApointment = lazy(() => import("./pages/Admin/AllApointment"));
-const AddDoctor = lazy(() => import("./pages/Admin/AddDoctor"));
-const DoctorsList = lazy(() => import("./pages/Admin/DoctorsList"));
-const DoctorDashboard = lazy(() => import("./pages/Doctor/DoctorDashboard"));
-const DoctorAppointments = lazy(() => import("./pages/Doctor/DoctorAppointments"));
-const DoctorProfile = lazy(() => import("./pages/Doctor/DoctorProfile"));
+import Dashboard from "./pages/Admin/Dashboard";
+import AllApointment from "./pages/Admin/AllApointment";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import { DoctorContext } from "./context/DoctorContext";
+import DoctorDashboard from "./pages/Doctor/DoctorDashboard";
+import DoctorAppointments from "./pages/Doctor/DoctorAppointments";
+import DoctorProfile from "./pages/Doctor/DoctorProfile";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
+  const { dToken } = useContext(DoctorContext);
 
   return (
-    <Router>
-      <ToastContainer />
-      {aToken ? (
+    <HashRouter>
+      {aToken || dToken ? (
         <div className="bg-[#F8F9FD]">
+          <ToastContainer />
           <Navbar />
           <div className="flex items-start">
             <Sidebar />
-            <Suspense fallback={<div>Loading...</div>}>
-              <Routes>
-                <Route path="/" element={<Dashboard />} />
-                <Route path="/admin-dashboard" element={<Dashboard />} />
-                <Route path="/all-appointments" element={<AllApointment />} />
-                <Route path="/add-doctor" element={<AddDoctor />} />
-                <Route path="/doctor-list" element={<DoctorsList />} />
-                <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
-                <Route path="/doctor-appointments" element={<DoctorAppointments />} />
-                <Route path="/doctor-profile" element={<DoctorProfile />} />
-              </Routes>
-            </Suspense>
+            <Routes>
+              <Route path="/" element={<></>} />
+              <Route path="/admin-dashboard" element={<Dashboard />} />
+              <Route path="/all-appointments" element={<AllApointment />} />
+              <Route path="/add-doctor" element={<AddDoctor />} />
+              <Route path="/doctor-list" element={<DoctorsList />} />
+              <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+              <Route path="/doctor-appointments" element={<DoctorAppointments />} />
+              <Route path="/doctor-profile" element={<DoctorProfile />} />
+            </Routes>
           </div>
         </div>
       ) : (
-        <Login />
+        <>
+          <Login />
+          <ToastContainer />
+        </>
       )}
-    </Router>
+    </HashRouter>
   );
 };
 
