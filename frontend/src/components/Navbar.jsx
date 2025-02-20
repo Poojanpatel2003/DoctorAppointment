0d7e06ee04c32f1ae2382a277fbe2639996d6c32
Poojanpatel2003 <poojanpatel1595@gmail.com>
2025-02-20 23:06:08 +0530

    final

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
     return (
         <div className="flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400 relative">
             {/* Logo */}
-            <img onClick={() => navigate("/")} className="w-44 cursor-pointer" src={assets.logo} alt="Logo" />
+            <img onClick={() => navigate("/")} className="w-50 cursor-pointer" src={assets.logo} alt="Logo" />
 
             {/* Desktop Navigation */}
             <ul className="hidden md:flex items-start gap-6 font-medium">
